6d50eea8b93a9ded418054198562fc0d5010bfb9
danielopes7 <daniel.lopes.amorim@hotmail.com>
2024-07-28 13:02:10 -0300

    up the repository model with migration

---
+++ app/Models/Repository.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Repository extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'language', 'repository_git_id', 'full_name', 'description', 'stargazers_count', 'forks', 'open_issues', 'watchers'
+    ];
+
+    /**
+     * Get the issues for the repository.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function Issues(): hasMany
+    {
+        return $this->hasMany(Issue::class);
+    }
+}
---
+++ database/migrations/2024_07_27_190100_create_repositories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('repositories', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('repository_git_id');
+            $table->string('language');
+            $table->string('full_name');
+            $table->text('description');
+            $table->integer('stargazers_count');
+            $table->integer('forks');
+            $table->integer('open_issues');
+            $table->integer('watchers');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('repositories');
+    }
+};
