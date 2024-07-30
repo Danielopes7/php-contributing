4d269629a9b2ac215e02be8a0c730cb3460119ef
danielopes7 <daniel.lopes.amorim@hotmail.com>
2024-07-29 21:21:42 -0300

    rm pattern singleton to call github api

--- app/Http/Controllers/GitHubController.php
+++ app/Http/Controllers/GitHubController.php
@@ -2,21 +2,47 @@
 
 namespace App\Http\Controllers;
 
-use App\Services\GitHubApi;
+use Illuminate\Http\Request;
+use Github\ResultPager;
+use Github\Client;
+use App\Services\IssueService;
+
+
 
 class GitHubController extends Controller
 {
-    protected $githubApi;
-
-    public function __construct(GitHubApi $githubApi)
+    protected $issueService;
+    public function __construct(IssueService $issueService)
     {
-        $this->githubApi = $githubApi;
+        $this->issueService = $issueService;
     }
 
-    public function searchIssues()
+    public function searchIssues(Client $client)
     {
-        $client = $this->githubApi->getClient();
-        $issues = $client->search()->issues('created:>2024-01-01 label:"good first issue" label:bug language:PHP');
+        $this->issueService->processUpdateSchedule();
+        dd('teste');
+        $pager = new ResultPager($client, 3);
+        $api = $client->search();
+        $method = 'issues';
+        $repo = $client->api('repo')->show('Submitty', 'Submitty');
+        dd($repo);
+        $parameters = ['created:>2024-01-01 label:"good first issue" label:bug language:PHP is:open comments:<2'];
+        
+        $issues = $pager->fetch($api, $method, $parameters);
+        dd($issues);
+        $teste = 0;
+        do {
+            // Processar as issues da página atual
+            foreach ($issues['items'] as $issue) {
+                // Faça algo com cada issue
+                echo $issue['title'] . "\n";
+            }
+        
+            // Buscar a próxima página de resultados
+            $issues = $pager->fetchNext();
+            $teste == $teste + 1;
+        } while ($teste < 2);
+        return view('index');
 
         return response()->json($issues);
     }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use App\Services\GitHubApi;
 use GrahamCampbell\GitHub\GitHubManager;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,10 +12,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->singleton(GitHubApi::class, function ($app) {
-            $github = $app->make(GitHubManager::class);
-            return GitHubApi::getInstance($github);
-        });
+        //
     }
 
     /**
