2cf2a6ec7f3e2b231811c0e4991f95d992108f7f
danielopes7 <daniel.lopes.amorim@hotmail.com>
2024-07-31 21:31:16 -0300

    adding future task that going to be used in scheduling

---
+++ app/Tasks/UpdateRecentIssues.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Tasks;
+use App\Services\IssueService;
+
+class UpdateRecentIssues
+{
+    protected $issueService;
+    public function __construct(IssueService $issueService)
+    {
+        $this->issueService = $issueService;
+    }
+    public function __invoke()
+    {
+        $this->issueService->processUpdateSchedule();
+    }   
+}
