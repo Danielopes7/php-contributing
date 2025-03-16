1aedb618afe288b3cdf1c4ada022400cacb492f4
danielopes7 <daniel.lopes.amorim@hotmail.com>
2025-03-15 21:35:15 -0300

    fix - Add title to layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>PHP Contributing</title>
     <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
   @vite('resources/css/app.css')
 
