bc2525ca659e0380e5ca210b564d16fd41f8a38a
danielopes7 <daniel.lopes.amorim@hotmail.com>
2025-03-26 22:23:33 -0300

    feat: Add GitHub Actions workflow for PHP static analysis and apply stan (#2)

--- app/Services/LabelService.php
+++ app/Services/LabelService.php
@@ -3,7 +3,6 @@
 namespace App\Services;
 
 use App\Models\Label;
-use Illuminate\Support\Facades\Log;
 
 class LabelService
 {
@@ -27,11 +26,6 @@ class LabelService
 
     public function deleteAll(): void
     {
-        try {
-            Label::query()->delete();
-            Log::info('All labels have been successfully deleted.');
-        } catch (\Exception $e) {
-            Log::error('Error deleting all labels: '.$e->getMessage());
-        }
+        Label::query()->delete();
     }
 }
--- routes/console.php
+++ routes/console.php
@@ -1,14 +1,8 @@
 <?php
 
 use App\Tasks\UpdateRecentIssues;
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Schedule;
-
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
 
 Schedule::call(function () {
     $task = resolve(UpdateRecentIssues::class);
